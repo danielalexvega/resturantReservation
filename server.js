a5f4acd12141d5dd08d784cacc63b25aa456d6f9
Daniel Vega <danielalexvega@gmail.com>
2020-12-24 13:41:58 -0600

    made some changes/ revisited this project

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,69 +15,50 @@ app.use(express.static('public'));
 
 // Reservations/Wait List (DATA)
 // =============================================================
-var reservations = [];
-var waitList = [];
+const reservations = [];
+const waitList = [];
 
 // Routes
 // =============================================================
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/makeReservation", function(req, res) {
+app.get("/makeReservation", function (req, res) {
   res.sendFile(path.join(__dirname, "makeReservation.html"));
 });
 
-app.get("/viewTables", function(req, res) {
-    res.sendFile(path.join(__dirname, "viewTables.html"));
-  });
+app.get("/viewTables", function (req, res) {
+  res.sendFile(path.join(__dirname, "viewTables.html"));
+});
 
 // Displays all reservations
-app.get("/api/reservations", function(req, res) {
+app.get("/api/reservations", function (req, res) {
   return res.json(reservations);
 });
 
 // Displays the waitlist
-app.get("/api/waitlist", function(req, res) {
-    return res.json(waitList);
-  });
+app.get("/api/waitlist", function (req, res) {
+  return res.json(waitList);
+});
 
-// // Displays a single reservation, or returns false
-// app.get("/api/reservations/:name", function(req, res) {
-//   var chosen = req.params.name;
 
-//   console.log(chosen);
-
-//   for (var i = 0; i < characters.length; i++) {
-//     if (chosen === characters[i].routeName) {
-//       return res.json(characters[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
-
-// Create New Characters - takes in JSON input
-app.post("/api/reservations", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
+app.post("/api/reservations", function (req, res) {
   var newReservation = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
   newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
 
-if(reservations.length >= 5) {
+  if (reservations.length >= 5) {
     waitList.push(newReservation);
-} else {
+  } else {
     reservations.push(newReservation);
-}
-    res.json(newReservation);
+  }
+  res.json(newReservation);
 });
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
